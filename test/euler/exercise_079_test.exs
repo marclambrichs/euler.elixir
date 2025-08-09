e269d462c5096c786a2ee1a58567213974020551
Marc Lambrichs <mlambrichs@melange-it.nl>
2025-08-09 19:18:10 +0200

    feat: Add exercise 79

---
+++ test/euler/exercise_079_test.exs
@@ -0,0 +1,12 @@
+defmodule Euler.Exercise_079_test do
+  use ExUnit.Case
+
+  import Euler.Exercise_079
+
+  describe "solutions/1" do
+    test "returns maximum total" do
+      solution()
+#      assert 7273 = solution()
+    end
+  end
+end
