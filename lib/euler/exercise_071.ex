92b76896d6f1453fd45bad0290e751a51d8da701
Marc Lambrichs <mlambrichs@melange-it.nl>
2025-08-13 22:59:59 +0200

    feat: Add exercise 71

---
+++ lib/euler/exercise_071.ex
@@ -0,0 +1,12 @@
+defmodule Euler.Exercise_071 do
+  @moduledoc """
+  Ordered Fractions
+
+  By listing the set of reduced proper fractions fpor d <= 1_000_000 in ascending order of size, find the numerator of the fraction
+  immediately left of 3/7
+  """
+  def timer(), do: :timer.tc(__MODULE__, :solution, [])
+
+  def solution() do
+  end
+end
