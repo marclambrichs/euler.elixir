d3518a18c8336a84a20a9fc77d9b249bcdd239e5
Marc Lambrichs <mlambrichs@melange-it.nl>
2025-08-17 18:16:41 +0200

    refactor: Add use of sieve of Eratosthenes

--- test/euler/exercise_069_test.exs
+++ test/euler/exercise_069_test.exs
@@ -8,4 +8,10 @@ defmodule Euler.Exercise_069_test do
       assert {30030, 5.213541666666666} = solution(100_000)
     end
   end
+
+  describe "solutions2/1" do
+    test "return totient maximum" do
+      assert 510_510 = solution2()
+    end
+  end
 end
