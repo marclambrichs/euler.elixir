db0525d7a1df5d9e58927d2a53d239b9959717e3
Marc Lambrichs <mlambrichs@melange-it.nl>
2025-09-10 10:45:29 +0200

    refactor: fix typo

--- lib/euler/exercise_071.ex
+++ lib/euler/exercise_071.ex
@@ -2,7 +2,7 @@ defmodule Euler.Exercise_071 do
   @moduledoc """
   Ordered Fractions
 
-  By listing the set of reduced proper fractions fpor d <= 1_000_000 in ascending order of size, find the numerator of the fraction
+  By listing the set of reduced proper fractions for d <= 1_000_000 in ascending order of size, find the numerator of the fraction
   immediately left of 3/7
   """
   def timer(), do: :timer.tc(__MODULE__, :solution, [])
