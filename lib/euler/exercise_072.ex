cc0cc11a99340a6e4a4032c4cdcd870f4e9031fd
Marc Lambrichs <mlambrichs@melange-it.nl>
2025-09-12 14:10:16 +0200

    feat: Add exercise 073

--- lib/euler/exercise_072.ex
+++ lib/euler/exercise_072.ex
@@ -5,16 +5,23 @@ defmodule Euler.Exercise_072 do
   How many elements would be contained in the set of reduced proper fractions for d <= 1_000_000?
   """
   alias Euler.Globals
+  alias Euler.Globals.Sequences.Sieve, as: S
   def timer(), do: :timer.tc(__MODULE__, :solution, [])
 
   def solution(n \\ 1_000_000) do
-    Stream.iterate(2, & &1 + 1)
-    |> Stream.map(& {&1, φ(&1)})
+    Stream.iterate(2, &(&1 + 1))
+    |> Stream.map(&{&1, φ(&1)})
     |> Stream.take_while(fn {a, _b} -> a <= n end)
     |> Enum.reduce(0, fn {_a, b}, acc -> acc + b end)
   end
 
+  def solution2(n \\ 1_000_000) do
+    Stream.scan(2..n, 0, &(φ(&1) + &2))
+    |> Enum.take(-1)
+  end
+
   def φ(n) when n > 1 do
-    Globals.primefactors(n) |> Enum.reduce(n, fn x, acc -> acc * (1 - 1 / x) end)
+    #  Globals.primefactors(n) |> Enum.reduce(n, fn x, acc -> acc * (1 - 1 / x) end)
+    S.primefactors(n) |> Enum.reduce(n, fn x, acc -> acc * (1 - 1 / x) end)
   end
 end
