eef02d9c79c8acb24d59a7ed97b3f40fca548e28
Marc Lambrichs <mlambrichs@melange-it.nl>
2025-09-15 21:53:31 +0200

    feat: Add exercise 075

---
+++ lib/euler/exercise_075.ex
@@ -0,0 +1,38 @@
+defmodule Euler.Exercise_075 do
+  @moduledoc """
+  Singular Integer Right Triangles
+
+  https://en.wikipedia.org/wiki/Tree_of_primitive_Pythagorean_triples#Alternative_methods_of_generating_the_tree
+  """
+  def timer(), do: :timer.tc(__MODULE__, :solution, [])
+
+  def solution(limit \\ 1_500_000) do
+    triples(limit)
+    |> Enum.reduce(Map.new(), fn tuple, cache ->
+      Map.update(cache, Tuple.sum(tuple), 1, &(&1 + 1))
+    end)
+    |> Enum.filter(fn {_k, v} -> v == 1 end)
+    |> Enum.count()
+  end
+
+  def triples(limit) do
+    primitive_triples({3, 4, 5}, limit)
+    |> Stream.flat_map(&transform(&1, limit))
+  end
+
+  def transform({a, b, c}, limit) do
+    Stream.duplicate({a, b, c}, div(limit, c))
+    |> Stream.scan(fn {a, b, c}, {x, y, z} -> {a + x, b + y, c + z} end)
+    |> Stream.filter(fn {a, b, c} -> a + b + c < limit end)
+    |> Stream.uniq()
+  end
+
+  def primitive_triples({a, b, c}, limit) when a + b + c > limit, do: []
+
+  def primitive_triples({a, b, c}, limit) do
+    [{a, b, c}] ++
+      primitive_triples({a + -2 * b + 2 * c, 2 * a - b + 2 * c, 2 * a - 2 * b + 3 * c}, limit) ++
+      primitive_triples({a + 2 * b + 2 * c, 2 * a + b + 2 * c, 2 * a + 2 * b + 3 * c}, limit) ++
+      primitive_triples({-a + 2 * b + 2 * c, -2 * a + b + 2 * c, -2 * a + 2 * b + 3 * c}, limit)
+  end
+end
